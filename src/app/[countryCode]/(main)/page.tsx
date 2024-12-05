a9a4b66f8742d4854df84c73719b083127a0c069
trog <trog@air.local>
2024-12-05 11:01:30 -0800

    update metadata

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,8 @@ import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
-  description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+  title: "TONKAS.WORLD",
+  description: "What's good America!",
 }
 
 export default async function Home(props: {
